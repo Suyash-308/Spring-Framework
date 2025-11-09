add1bb6725435d20e2457eb27ea05ed55b641060
Suyashnichit308 <nichitsuyash@gmail.com>
2025-11-09 14:10:15 +0530

    core configuration

---
+++ spring-core_java-configuration/src/main/java/com/itshaala/model/Employee.java
@@ -0,0 +1,14 @@
+package com.itshaala.model;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Employee {
+private int id;
+private String name;
+private double salary;
+}
